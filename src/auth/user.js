27ebb9fce7f839dccf7b02c1615043d9527390e6
athiththan <90118973+athiththan@users.noreply.github.com>
2021-09-30 15:04:32 +0530

    Update user.js

--- src/auth/user.js
+++ src/auth/user.js
@@ -1,93 +1,53 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import InfoTable from "./infoTable";
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import InfoForm from './infoForm';
 
 class User extends React.Component
 {
-  constructor(props)
+  constructor()
   {
-     super(props);
-    // this.setState = {
-    //   BookName:"",
-    //   Authername:"",
-    //   price:""
-  //   }
-  // }
-  // infoChange = event=>
-  // {
-  //   const { name,value } = event.target;
-  //   this.setState({
-  //     [name] : value
-  //   })
-  // }
-  // infoEdit = event =>
-  // {
-  //   console.log(this.state.BookName);
+    super();
+    this.state = {
+      data:[]
+    }
   }
-  render(){
-  return(
-    <>
-    <form onEdit = {this.infoEdit}>
+   create = data =>
+        {
+         axios.post("http://localhost:8080/Api/addbooks",data).then(res => {
+           console.log(res);
+         })
+        }
+  componentDidMount()
+  {
+     this.getAll();
+  }
+  getAll()
+  {
+    axios.get("http://localhost:8080/Api/getAll").then(res => {
+      console.log(res.data);
+      this.setState({
+        data:res.data
+      })
+        
+      })
     
-      <label for="Book Name"> Book Name</label>
-      <input type="text" className="form-control" placeholder="Enter Book Name" 
-        //onChange ={this.infoChange} name="BookName" value={this.state.BookName}
-        />
-    
-    <label for="Auther Name"> Auther Name</label>
-      <input type="text" className="form-control" placeholder="Enter Auther Name" 
-       //onChange ={this.infoChange} name="AutherName" value={this.state.AutherName}
-       />
-        <label for="Book Price"> Book Price</label>
-      <input type="text" className="form-control" placeholder="Enter Book Price" 
-       //onChange ={this.infoChange} name="BookPrice" value={this.state.price}
-       />
-    
-    <button type="button" className="btn btn-primary mt-3" >Add</button>
-  </form>
-
-
-  <table className="table">
-  <thead>
-    <tr>
-      <th>S.No</th>
-      <th>Book List</th>
-      <th>Auther Name</th>
-      <th>Edit</th>
-      <th>Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>dir</td>
-      <td>dir</td>
-      <td><button onClick = {this.infoChange}
-      className = "btn btn-primary">Edit</button></td>
-      <td><button className="btn btn-secondary">Remove</button></td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Moe</td>
-      <td>dir</td>
-      <td><button className="btn btn-primary">Edit</button></td>
-      <td><button className="btn btn-secondary">Remove</button></td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Dooley</td>
-      <td>dir</td>
-      <td><button className="btn btn-primary">Edit</button></td>
-      <td><button className="btn btn-secondary">Remove</button></td>
-    </tr>
-  </tbody>
-</table>
-
-
-     //<button onClick = {()=>{
-       //localStorage.clear();
-      // props.history.push('/login')
-     }} className = "btn btn-primary"> logout</button>
-     </>
-   ) 
- }
+  }
+  render()
+  {
+   return(
+    <div className='container mt-3'>
+      <div className='row'>
+         
+           <InfoForm myData ={this.create}/>
+         
+         
+        <InfoTable getData = {this.state.data}/>
+      
+    </div>
+    </div>
+   );
+  }   
 }
 export default User;
